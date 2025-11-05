e6e90f73b2627d15bdd386f4dff65beb916b7523
AQUIB IRFANI <maquib1710@gmail.com>
2025-11-05 19:08:09 +0530

    Restrict CORS to specific frontend origin

--- finale/final_backend/app.js
+++ finale/final_backend/app.js
@@ -5,7 +5,9 @@ const cors = require("cors");
 const app = express()
 connectMongo()
 
-app.use(cors())
+app.use(cors({
+  origin: "https://blog-app-frontend-rxob.onrender.com"
+}));
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/blog', routes)
